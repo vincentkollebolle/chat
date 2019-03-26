350e7a20c466055b548e5e36b63d62ed528515b7
Vincent B <vincent@kollebolle.com>
2019-03-26 15:57:06 +0100

    ajout server node

---
+++ index.js
@@ -0,0 +1,22 @@
+let express = require('express')
+let app = express();
+
+let http = require('http');
+let server = http.Server(app);
+
+let socketIO = require('socket.io');
+let io = socketIO(server);
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('new-message', (message) => {
+        io.emit('new-message', message);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
