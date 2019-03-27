6ad3e8a83a577b4994e9cc7f4bc5af8919191694
BILY Victorien <victorien.bily@altran.com>
2019-03-27 09:45:21 +0100

    'evol'

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-let express = require('express')
-let app = express();
+const port = '3000'
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+var users = [];
 
-let http = require('http');
-let server = http.Server(app);
-
-let socketIO = require('socket.io');
-let io = socketIO(server);
-
-const port = process.env.PORT || 3000;
-
-io.on('connection', (socket) => {
-    console.log('user connected');
-
-    socket.on('new-message', (message) => {
-        io.emit('new-message', message);
+// Quelqu'un rejoint le socket
+io.on('connection', function(socket){
+    console.log('User is connected');
+    // On lui envoie les utilisateurs présents sur le chat
+    io.emit('allUsers', users);
+    // Il demande à rejoindre le chat avec un pseudo
+    socket.on('newUser', function (pseudo){
+      // On stock le pseudo sur la session du serveur
+      socket.pseudo = pseudo;
+      // On ajoute l'utilisateur à la liste des utilisateurs présents
+      users.push(pseudo);
+      // On envoie un message aux autres utilisateurs avec son pseudo
+      io.emit('newUser', pseudo);
+      // On met à jour la liste des utilisateurs présents pour tous le monde
+      io.emit('allUsers', users);
+    })
+    // Il envoie un message
+    socket.on('message', function (message) {
+      // On envoie le message aux autres utilisateurs avec son pseudo récupéré dans la session du serveur
+      io.emit('message', {pseudo: socket.pseudo, message: message});
     });
-});
+    // Il se deconnecte mais reste sur la page (socket toujours présent)
+    socket.on('logout', function (message) {
+      // On le supprime de la liste des utilisateurs
+      users.pop(socket.pseudo);
+      // On envoie un message aux autres utilisateurs pour prévenir la déconnexion
+      io.emit('logout', {pseudo: socket.pseudo, message: message});
+      // On mmet à jour la liste des utilisateurs présents pour tout le monde
+      io.emit('allUsers', users);
+    }); 
+    // Il quitte le navigateur
+    socket.on('disconnect', function(){
+      console.log('User is disconnected');
+    });
+  });
 
-server.listen(port, () => {
-    console.log(`started on port: ${port}`);
+http.listen(port, function(){
+    console.log('listening on port : '+ port);
 });
